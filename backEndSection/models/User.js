5d75fb6ae5573c6daa7345a584d247056e6b688c
Yogesh Reddy <yogeshreddy003@gmail.com>
2025-12-16 23:17:01 +0530

    Update app to index

--- backEndSection/models/User.js
+++ backEndSection/models/User.js
@@ -1,24 +1,12 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: [true, "Email must be unique"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-  address: {
-    type: String,
-    default: "",
-  },
-});
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", userSchema);
-export default User;
+export default mongoose.model("User", userSchema);
