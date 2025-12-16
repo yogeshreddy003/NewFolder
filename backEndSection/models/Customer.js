88da37b6ddc64f5720cc01962e1561beec2acbf9
Yogesh Reddy <yogeshreddy003@gmail.com>
2025-12-16 22:51:26 +0530

    Update app to index

--- backEndSection/models/Customer.js
+++ backEndSection/models/Customer.js
@@ -1,11 +1,21 @@
 import mongoose from "mongoose";
 
-const customerSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  phone: String,
-  message: String,
-});
+const customerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const Customer = mongoose.model("signup", customerSchema);
+const Customer = mongoose.model("Customer", customerSchema);
+
 export default Customer;
