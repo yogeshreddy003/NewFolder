c7738c8e0c67e1c7e828575bfae6b213f430242c
Yogesh Reddy <yogeshreddy003@gmail.com>
2025-12-19 16:22:38 +0530

    1st change 2nd way

--- backEndSection/controllers/userController.js
+++ backEndSection/controllers/userController.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 // Helper to generate token
 const generateToken = (id) => {
--- backEndSection/models/User.js
+++ backEndSection/models/User.js
@@ -14,5 +14,5 @@ password: {
   },
   { timestamps: true }
 );
-
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+export default User;
